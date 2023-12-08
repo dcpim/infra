3cda4b0218b9560dda1924c3f3ea0a445d6da835
Patrick Lambert <152328019+dcpim@users.noreply.github.com>
2023-12-08 10:52:26 -0500

    Added subnet, secret and db info

--- variables.tf
+++ variables.tf
@@ -2,6 +2,30 @@ variable "region" {
   default = "us-west-2"
 }
 
+variable "env" {
+  default = "test"
+}
+
 variable "cidr" {
   default = "10.5.0.0/16"
+}
+
+variable "cidr_priv1" {
+  default = "10.5.1.0/24"
+}
+
+variable "cidr_priv2" {
+  default = "10.5.2.0/24"
+}
+
+variable "cidr_pub" {
+  default = "10.5.3.0/24"
+}
+
+variable "secret_arn" {
+  default = "arn:aws:secretsmanager:us-west-2:310790002532:secret:dcpim-test-e6pBOP"
+}
+
+variable "db_instance_type" {
+  default = "db.t4g.medium"
 }
