3ea1989e149635a2dec4a9153af992c391ba1f1e
Patrick Lambert <152328019+dcpim@users.noreply.github.com>
2023-12-11 20:48:48 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -50,3 +50,37 @@ resource "aws_subnet" "dcpim_subnet_pub" {
     Name = "dcpim-subnet-pub-${var.env}"
   }
 }
+
+# S3 buckets
+resource "aws_s3_bucket" "dcpim_s3_media" {
+  bucket = "dcpim-media-${var.env}"
+}
+
+resource "aws_s3_bucket_versioning" "dcpim_s3_media_version" {
+  bucket = aws_s3_bucket.dcpim_s3_media.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "dcpim_s3_media_sse" {
+  bucket = aws_s3_bucket.dcpim_s3_media.id
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
+}
+
+resource "aws_s3_bucket_lifecycle_configuration" "dcpim_s3_media_lifecycle" {
+  bucket = aws_s3_bucket.dcpim_s3_media.id
+  rule {
+    id = "delete-expire-after-30-days"
+    status = "Enabled"
+    noncurrent_version_expiration {
+        newer_noncurrent_versions = 0
+        noncurrent_days = 30
+      }
+    }
+  }
+}
